e1c6878593c72801941f6cfd3b07a254d08bfda8
Matvey <morozovmatvej75@gmail.com>
2022-11-19 12:18:39 +0300

    router test 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello, world!</h1>
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 )
