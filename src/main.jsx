ec2215a851e7c17ace54b24c3ebbca3cb0d1bf70
Matvey <morozovmatvej75@gmail.com>
2022-11-19 12:42:59 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
 import {
   createBrowserRouter,
   RouterProvider,
   Route
 } from "react-router-dom"
+import Root from "./routes/root"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Hello, world!</h1>
+    element: <Root />
   }
 ])
 
-const root = ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
