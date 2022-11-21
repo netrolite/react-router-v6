c1d0ccdb38d83e29cbee688d2df08eeefc381a0f
Matvey <morozovmatvej75@gmail.com>
2022-11-21 20:07:54 +0300

    go to homepage from profile page

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom"
 
 export default function Profile() {
     const { username } = useParams();
+    const navigate = useNavigate();
 
     return (
-        <h1>This is {username}'s profile page</h1>
+        <>
+            <h1>This is {username}'s profile page</h1>
+            <button onClick={() => navigate("/")}>Homepage</button>
+        </>
     )
 }
