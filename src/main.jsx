64c83d7cbc163ea685a7dec83af337c1dac2a802
Matvey <morozovmatvej75@gmail.com>
2022-11-21 18:41:27 +0300

    add routes for 4 pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route
-} from "react-router-dom"
-import Root, { loader as rootLoader } from "./routes/root"
-import ErrorPage from "./errorPage"
-import Contact from "./routes/contact"
+import App from "./App"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )
