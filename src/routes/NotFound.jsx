3c03797322aa8edf5bf838c69bf4651c5013b965
Matvey <morozovmatvej75@gmail.com>
2022-11-21 18:49:36 +0300

    not found page

---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <>
+            <h1>Page not found!</h1>
+            <i>404 error</i>
+        </>
+    )
+}
