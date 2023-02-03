4061f2052ab532ea382dbb2460c48018c20e630c
Matvey <morozovmatvej75@gmail.com>
2023-02-03 07:02:52 +0300

    wipe everything out and use vite

--- src/pages/About.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-	return (
-    <div className="about">
-      <h2>About route</h2>
-      <p>inwonwefoiwenf unownegoinweo inoiwnoiwengoweng owoenweobweovn owneoiwen</p>
-    </div>
-	)
-}
--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="home">
-      <h2>Home route</h2>
-      <p>owoenweobweovn owneoiwenowoenweobweovn owneoiwen vwbeiwuwe</p>
-    </div>
-  )
-}
