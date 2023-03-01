a2c273852b7c666cc44b14d2d6117fa46ec65b98
Matvey <morozovmatvej75@gmail.com>
2023-03-01 16:28:53 +0300

    replace root url with "Home" in breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -3,27 +3,31 @@ import { useLocation, Link } from "react-router-dom"
 
 export default function Breadcrumbs() {
   const location = useLocation();
-  console.log(location);
-
-  console.log("before");
-  let currentLink = "";
-  const breadcrumbsNodes = location.pathname.split("/")
-    .filter(crumb => crumb !== "")
-    .map(crumb => {
-      currentLink += `/${crumb}`;
-      console.log(currentLink);
-      return (
-        <li className="breadcrumb">
-          <Link to={currentLink}>
-            {crumb}
-          </Link>
-        </li>
-      )
-    })
+  const breadcrumbsNodes = getBreadcrumbs(location);
 
   return (
     <div className="breadcrumbs-container">
       <ul className="breadcrumbs">{breadcrumbsNodes}</ul>
     </div>
   )
+}
+
+function getBreadcrumbs(location: ReturnType<typeof useLocation>) {
+  const { pathname } = location;
+  const splitPath = pathname.split("/");
+  // if pathname is root, the splitPath is ["", ""]. So we remove the last emtpy string
+  if (pathname === "/") splitPath.pop();
+  
+  let currentLink = "";
+  return splitPath.map((pathPart, i) => {
+    currentLink += `/${pathPart}`;
+    
+    return (
+      <li className="breadcrumb" key={i}>
+        <Link to={currentLink}>
+          {pathPart === "" ? "Home" : pathPart}
+        </Link>
+      </li>
+    )
+  })
 }
